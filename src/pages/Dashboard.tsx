fbda8a2f218358424c23b6d566f75753126f183e
chingakhamronald <ronaldoch.lamzing@gmail.com>
2023-01-27 12:30:30 +0530

    fetch data using react query

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,55 @@
-import {Text, View} from 'react-native';
+import {Text, ToastAndroid, View} from 'react-native';
 import React, {FC} from 'react';
 import {styleMain} from '../styles';
-import {FlatList, ScrollView} from 'react-native-gesture-handler';
+import {
+  FlatList,
+  RefreshControl,
+  ScrollView,
+} from 'react-native-gesture-handler';
+import {API} from '../constant';
+import axios from 'axios';
+import {useQuery} from 'react-query';
+import {ActivityIndicator} from 'react-native-paper';
 
 const Dashboard: FC = () => {
+  const getData = async () => {
+    return await axios
+      .get(API + 'users')
+      .then(res => {
+        return JSON.parse(res as any);
+      })
+      .catch(er => er);
+  };
+
+  const {isLoading, data, error, isError} = useQuery('test', getData);
+
+  console.log({data});
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        color="red"
+        size={'small'}
+        style={[
+          styleMain.flex,
+          {justifyContent: 'center', alignItems: 'center'},
+        ]}
+      />
+    );
+  }
+
+  if (isError) {
+    return ToastAndroid.show(error as any, ToastAndroid.SHORT);
+  }
+
   return (
-    <ScrollView style={styleMain.flex}>
+    <ScrollView
+      style={styleMain.flex}
+      refreshControl={
+        <RefreshControl refreshing={isLoading} onRefresh={getData} />
+      }>
       <View>
-        <Recent />
+        <Recent recentData={data} />
       </View>
     </ScrollView>
   );
@@ -15,9 +57,10 @@ const Dashboard: FC = () => {
 
 export default Dashboard;
 
-const Recent = () => {
+const Recent = ({recentData}) => {
   return (
     <FlatList
+      // data={recentData}
       ListHeaderComponent={() => {
         return (
           <View>
